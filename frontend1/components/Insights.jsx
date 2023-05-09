85167d978c732c5de2ca7541509067704c219a21
variehira <variehira@gmail.com>
2023-05-09 20:31:00 +0700

    insights1

--- frontend1/components/Insights.jsx
+++ frontend1/components/Insights.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Insights = () => {
   return (
-    <h1 className="head_text text-center">
+    <div className="head_text text-left md:mt-32 md:pr-80 md:ml-10">
         We are the global leader in commercial real estate services and investments.    
-        <span className="head_text_grey text-center"> With services, insights and data that span every dimension of the industry,
+        <span className="head_text_grey"> With services, insights and data that span every dimension of the industry,
         we create solutions for clients of every size,
-        in every sector and across every geography.
+        in every sector and across every geography.<br/>
         </span>
-    </h1>
+        <br/><br/>
+        <Link href="/" className="head_text_grey underline">
+        Learn More
+        </Link>
+    </div>
   )
 }
 
